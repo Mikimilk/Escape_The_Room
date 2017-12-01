dd4134df6675ebee6d735057349b04086de85b91
Ravin <milknaiti@hotmail.com>
2017-12-01 13:56:38 +0700

    charactor movement

---
+++ core/src/com/milk/gdx/game/GameScreen.java
@@ -0,0 +1,49 @@
+package com.milk.gdx.game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class GameScreen extends ScreenAdapter{
+	private EscapeTheRoom EscapeGame;
+    private Texture charactorImg;
+    private int x;
+    private int y;
+	 
+    public GameScreen(EscapeTheRoom EscapeGame) {
+        this.EscapeGame = EscapeGame;
+        charactorImg = new Texture("C:\\Users\\user\\JavaGame\\EscapeTheRoom\\core\\assets\\charactor_01.png");
+        x = 100;
+        y = 100;
+    }
+    private void update(float delta) {
+    	if(Gdx.input.isKeyPressed(Keys.LEFT)) {
+            x -= 5;
+        }
+        if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
+            x += 5;
+        }
+        if(Gdx.input.isKeyPressed(Keys.UP)) {
+            y += 5;
+        }
+        if(Gdx.input.isKeyPressed(Keys.DOWN)) {
+            y -= 5;
+        }
+    }   
+    
+    @Override
+    public void render(float delta) {
+    	Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+    	update(delta);
+    	SpriteBatch batch = EscapeGame.batch;
+        batch.begin();
+        batch.draw(charactorImg, x, y);
+        batch.end();
+        
+    }
+}
+
---
+++ desktop/src/com/milk/gdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.milk.gdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.milk.gdx.game.EscapeTheRoom;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 600;
+		new LwjglApplication(new EscapeTheRoom(), config);
+	}
+}
